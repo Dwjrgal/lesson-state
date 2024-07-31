a1a643e6d35196da42744cdb793d82df676cd2fe
dwjrgal <dwjrgl651@gmail.com>
2024-07-31 13:00:27 +0800

    clear button

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,26 +1,14 @@
 import React, { useState } from "react";
 
-const Input = () => {
-  const [searchValue, setSearchValue] = useState("");
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setSearchValue(e.target.value);
-  };
-
-  const handleClick = (e) => {
-    console.log("clicked");
-  };
-
+const Input = ({ handleChange }) => {
   return (
     <>
       <input
-        className="border border-gray-400"
+        className="border border-green-800 rounded p-1 "
         type="text"
         onChange={handleChange}
       ></input>
-      <p> Search value: {searchValue}</p>
-      <button onClick={handleClick}>click</button>
+      {/* <p className="text-center"> Search value: {searchValue}</p> */}
     </>
   );
 };
